79aaccf51d46d51b4fd7f401cebb75b753bcd99a
Vladimir Karmanov <vladimir1998karmanov@gmail.com>
2024-12-30 18:19:53 +0500

    Add users handler

--- bot/handlers/admin/router.py
+++ bot/handlers/admin/router.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 
-from handlers.admin.base import router as base_router
+from handlers.admin.panel import router as panel_router
+from handlers.admin.users import router as users_router
 from middlewares.auth import IsAdminMiddleware
 from settings import settings
 
@@ -10,4 +11,4 @@ if settings.is_prod:
     router.message.middleware.register(IsAdminMiddleware())
     router.callback_query.middleware.register(IsAdminMiddleware())
 
-router.include_routers(base_router)
+router.include_routers(panel_router, users_router)
