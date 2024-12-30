acb9babd01bdc580f7d814c2a91fa01389a34b02
Vladimir Karmanov <vladimir1998karmanov@gmail.com>
2024-12-30 21:49:07 +0500

    Update start handler

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -2,8 +2,9 @@ from aiogram import Router, types
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 
-from constants.commands import side_menu
+from constants.commands import menu, side_menu
 from schemas.keyboard import ButtonSchema
+from settings import settings
 from utils.keyboard import get_main_menu_keyboard
 
 router = Router()
@@ -12,9 +13,13 @@ router = Router()
 @router.message(Command(side_menu['start'].command))
 async def start(message: types.Message, state: FSMContext):
     await state.clear()
-    buttons = [ButtonSchema(text='Рандом')]
+    buttons = [ButtonSchema(text=b.button_text) for b in menu.values()]
+    text = (
+        f'Подберу рандомную тренировку в стиле FYSM /random\n\n'
+        f'Оставить обратную связь или предложения @{settings.DEVELOPER_USERNAME}'
+    )
     await message.answer(
-        text='Подберу рандомную тренировку в стиле FYSM\n\n' 'Жми /random',
+        text=text,
         parse_mode='HTML',
         reply_markup=get_main_menu_keyboard(buttons),
     )
