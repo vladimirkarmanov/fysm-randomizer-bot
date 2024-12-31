4a9865bec630964d2305fd0ef5c1cdb81f07c5af
Vladimir Karmanov <vladimir1998karmanov@gmail.com>
2024-12-31 15:45:48 +0500

    Refactoring

--- bot/handlers/feedback.py
+++ bot/handlers/feedback.py
@@ -2,8 +2,7 @@ from aiogram import Router, types
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 
-from constants.commands import menu, side_menu
-from schemas.keyboard import ButtonSchema
+from constants.commands import side_menu
 from settings import settings
 from utils.keyboard import get_main_menu_keyboard
 
@@ -13,10 +12,9 @@ router = Router()
 @router.message(Command(side_menu['feedback'].command))
 async def feedback(message: types.Message, state: FSMContext):
     await state.clear()
-    buttons = [ButtonSchema(text=b.button_text) for b in menu.values()]
     text = f'Для обратной связи и предложений -> @{settings.DEVELOPER_USERNAME}'
     await message.answer(
         text=text,
         parse_mode='HTML',
-        reply_markup=get_main_menu_keyboard(buttons),
+        reply_markup=get_main_menu_keyboard(),
     )
